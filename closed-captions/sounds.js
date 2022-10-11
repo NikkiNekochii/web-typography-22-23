79ccfdfd834058bf803c6ae4594897f4f6239a28
NikkiNekochii <105801253+NikkiNekochii@users.noreply.github.com>
2022-10-11 15:03:35 +0200

    sound0
    moving gradient from left to right

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,9 +1,10 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
+	0, /* alsof iet voorbij vliegt */
+	3,
+	9, /* celdeur open alarm */
+	12, 
 	15,
 	16.5,
 	24,
