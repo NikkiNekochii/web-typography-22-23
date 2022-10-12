d6f7131628f6d95f619568eaeee35ba9ab156d6f
NikkiNekochii <105801253+NikkiNekochii@users.noreply.github.com>
2022-10-12 09:59:56 +0200

    Sounds
    Beschrijving van de geluiden zodat ik weet wat wat is.

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -2,15 +2,15 @@
 // Op die mommenten komt er een class op de body.
 sounds = [
 	0, /* alsof iet voorbij vliegt */
-	3, // loze ruimte
+	3, // loze ruimte opvullen geen geluid
 	9, /* celdeur open alarm */
-	12, 
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	12, // alarm politie
+	15, // buzzing noice
+	16.5, // microfoon glitch
+	24, // buzzing noice 2
+	25.8, // microfoon glitch 2
+	31, // buzzing noice 3 more profound
+	32, // microfoon glitch 3 more profound
+	34, // piep geluid dat steeds harder word
+	86 // mysterious space music
 ];
