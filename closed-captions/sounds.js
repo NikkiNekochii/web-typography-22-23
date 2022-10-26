980dacb0d0c47e02262513c98f677d900a449df7
NikkiNekochii <105801253+NikkiNekochii@users.noreply.github.com>
2022-10-26 14:21:54 +0200

    Update sounds.js

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -5,11 +5,11 @@ sounds = [
 	3, // loze ruimte opvullen geen geluid
 	9, /* celdeur open alarm */
 	12, // alarm politie
-	15, // buzzing noice
+	15, // buzzing noise
 	16.5, // megafoon glitch
-	23.9, // buzzing noice 2
+    23.9, // buzzing noise 2
 	25.8, // megafoon glitch 2
-	31, // buzzing noice 3 more profound
+	31, // buzzing noise 3 more profound
 	32, // megafoon glitch 3 more profound
 	33, // piep geluid dat steeds harder word
 	86 // mysterious space music
